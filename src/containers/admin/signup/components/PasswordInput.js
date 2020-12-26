169e37830fe22461993d350740f34741d4e8fcdc
unknown <techwithuche@gmail.com>
2020-12-26 17:09:39 +0100

    lo

--- src/containers/admin/signup/components/PasswordInput.js
+++ src/containers/admin/signup/components/PasswordInput.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
